3d8107fbdd29683a49f78d29142813b14e3a2571
fs <fsbarretofilho@gmail.com>
2026-05-03 16:26:56 -0300

    feat: add novas cores

--- include/Estilo.hpp
+++ include/Estilo.hpp
@@ -5,12 +5,17 @@
 
 namespace Estilo{
 
+    const std::string DESTAQUE = "\033[7m";
     const std::string RESET = "\033[0m";
     const std::string CIANO = "\033[36m";   
     const std::string MAGENTA = "\033[35m";   
     const std::string AMARELO = "\033[33m";   
     const std::string VERMELHO = "\033[31m";   
-    const std::string VERDE = "\033[32m";   
+    const std::string VERDE = "\033[32m";
+    const std::string AZUL = "\033[94m";
+    const std::string ROSA = "\033[95m";
+    const std::string VERDE_CLARO = "\033[92m";
+    const std::string CINZA_ESCURO = "\033[90m";   
 
     void impressaoEscrita(const std::string& texto, int delayMs = 30);
     void limpaTela();
