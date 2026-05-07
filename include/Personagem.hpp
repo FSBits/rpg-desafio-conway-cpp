ed0bf1c03e4a38adf75d8e8a64f2f55a6a6fe253
fs <fsbarretofilho@gmail.com>
2026-05-07 18:37:36 -0300

    feat: implementa um sistema de inv simples
    cria o inv e refatora personagem para comportar nova feat.

--- include/Personagem.hpp
+++ include/Personagem.hpp
@@ -3,7 +3,24 @@
 
 #include "Entidade.hpp"
 #include <string>
+#include <vector>
 
+enum IdItem{
+
+    ARMA,
+    CONSUMIVEL,
+    CHAVE
+};
+
+struct Item{
+
+    std::string nome;
+    std::string descricao;
+    IdItem id;
+    int valorEfeito; 
+    int quantidade;
+    bool armaDeFogo;
+};
 class Personagem : public Entidade{
     
     protected:
@@ -18,11 +35,18 @@ class Personagem : public Entidade{
         int sorte;
         int carisma;
 
+        std::vector<Item> inv;
+        Item armaEquipada;
+
     public:
 
         Personagem(std::string t_nome, std::string t_classe, int t_forca, int t_reflexos, int t_inteligencia, int n_tecnica, int t_moral, int t_sorte, int t_carisma);
         
         void ficha();
+        void adcionarItem(Item novoItem);
+        void listarInv();
+        bool usarItem(int index);
+        bool temItem(std::string nomeItem);
 
         int getForca();
         int getReflexos();
@@ -31,6 +55,8 @@ class Personagem : public Entidade{
         int getMoral();
         int getSorte();
         int getCarisma();
+        Item getArmaEquipada();
+        Item getArmaDeFogo();
 };
 
 #endif
--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -5,9 +5,100 @@
 Personagem::Personagem(std::string t_nome, std::string t_classe, int t_forca, int t_reflexos, int t_inteligencia, int t_tecnica, int t_moral, int t_sorte, int t_carisma)
     : Entidade(t_nome, 100 + (t_forca * 5)), 
       classe(t_classe), forca(t_forca), reflexos(t_reflexos), inteligencia(t_inteligencia), 
-      tecnica(t_tecnica), moral(t_moral), sorte(t_sorte), carisma(t_carisma) {}
+      tecnica(t_tecnica), moral(t_moral), sorte(t_sorte), carisma(t_carisma) {
 
-void Personagem::ficha() {
+        Item pistola = {"3 OITAO", "Arma de fogo basica.", ARMA, 5, 1, true};
+        Item estimulante = {"estimulante", "Injecao de cura rapida.", CONSUMIVEL, 30, 3, false};
+
+        armaEquipada = pistola;
+        adcionarItem(pistola);
+        adcionarItem(estimulante);
+}
+
+
+void Personagem::adcionarItem(Item novoItem){
+
+    int i;
+    for(i = 0; i < inv.size(); i++){
+
+        if(inv[i].nome == novoItem.nome){
+            
+            inv[i].quantidade += novoItem.quantidade;
+            Estilo::impressaoEscrita(Estilo::VERDE_CLARO + "[SISTEMA] + " + std::to_string(novoItem.quantidade) + "x " + novoItem.nome + " (Total: " + std::to_string(inv[i].quantidade) + ")" + Estilo::RESET + "\n", 50);
+            return;
+        }
+    }
+    
+    inv.push_back(novoItem);
+    Estilo::impressaoEscrita(Estilo::VERDE_CLARO + "[SISTEMA] Novo item obtido: " + novoItem.nome + Estilo::RESET + "\n", 30);
+}
+
+void Personagem::listarInv(){
+
+    Estilo::impressaoEscrita("\n" + Estilo::AMARELO + ">>> MOCHILA DE EQUIPAMENTOS <<<" + Estilo::RESET + "\n", 50);
+
+    int i;
+    for(i = 0; i < inv.size(); i++){
+
+        if(inv[i].quantidade > 0){
+
+            Estilo::impressaoEscrita("[" + std::to_string(i) + "] " + std::to_string(inv[i].quantidade) + "x " + Estilo::CIANO + inv[i].nome + Estilo::RESET, 25);
+
+            if(inv[i].id == ARMA){
+                Estilo::impressaoEscrita(Estilo::MAGENTA + " (Dano: " + std::to_string(inv[i].valorEfeito) + ")\n" + Estilo::RESET, 25);
+            } else if(inv[i].id == CONSUMIVEL)
+                Estilo::impressaoEscrita(Estilo::MAGENTA + " (Cura: " + std::to_string(inv[i].valorEfeito) + " HP)\n" + Estilo::RESET, 25);
+              else
+                Estilo::impressaoEscrita(Estilo::MAGENTA + " (Item Chave)\n" + Estilo::RESET, 25);
+        }
+        Estilo::impressaoEscrita("  > " + Estilo::CINZA_ESCURO + inv[i].descricao + Estilo::RESET + "\n", 20);
+    }
+}
+
+bool Personagem::usarItem(int index){
+
+    if(index > 0 && index < inv.size() && inv[index].quantidade > 0){
+
+        Item& itemEscolhido = inv[index];
+
+        switch(itemEscolhido.id){
+
+            case ARMA:
+                armaEquipada = itemEscolhido;
+                Estilo::impressaoEscrita(Estilo::VERDE_CLARO + "[Sistema] " + itemEscolhido.nome + " equipado.\n" + Estilo::RESET, 10);
+                return true;
+            
+            case CONSUMIVEL:
+                curar(itemEscolhido.valorEfeito);
+                itemEscolhido.quantidade--;
+                Estilo::impressaoEscrita(Estilo::VERDE_CLARO + "[Sistema] Voce usou " + itemEscolhido.nome + ". (+ " + std::to_string(itemEscolhido.valorEfeito) + " HP)\n" + Estilo::RESET, 10);
+                return true;
+            
+            case CHAVE:
+                Estilo::impressaoEscrita(Estilo::VERMELHO + "[Sistema] Nao ha onde usar " + itemEscolhido.nome + " aqui. Tente usar no cenario correto.\n" + Estilo::RESET, 10);
+                return false;
+            
+            default:
+                Estilo::impressaoEscrita(Estilo::VERMELHO + "[ERRO] Entrada invalida. " + Estilo::RESET, 10);
+                return false;
+        }
+    }
+
+    Estilo::impressaoEscrita(Estilo::VERMELHO + "[ERRO] Item invalido ou esgotado.\n" + Estilo::RESET, 10);
+    return false;
+}
+
+bool Personagem::temItem(std::string nomeItem){
+
+    int i;
+    for(i = 0; i < inv.size(); i++){
+
+        if(inv[i].nome == nomeItem && inv[i].quantidade > 0) return true;
+    }
+    return false;
+}
+
+void Personagem::ficha(){
     Estilo::impressaoEscrita(Estilo::CIANO + "=======================================", 10);
     Estilo::impressaoEscrita(Estilo::DESTAQUE + Estilo::AMARELO + "       REGISTRO DE MERCENARIO          " + Estilo::RESET, 10);
     Estilo::impressaoEscrita(Estilo::CIANO + "=======================================" + Estilo::RESET, 10);
@@ -20,10 +111,10 @@ void Personagem::ficha() {
 
     Estilo::impressaoEscrita(Estilo::CIANO+ "=======================================\n" + Estilo::RESET, 5);
     
-    Estilo::impressaoEscrita(Estilo::AZUL + "FORCA:        " + Estilo::RESET + std::to_string(forca) + " (Impacto fisico e resistencia)", 10);
+    Estilo::impressaoEscrita(Estilo::AZUL + "FORCA:        " + Estilo::RESET + std::to_string(forca) + " (Combate fisico e resistencia)", 10);
     Estilo::impressaoEscrita(Estilo::AZUL + "REFLEXOS:     " + Estilo::RESET + std::to_string(reflexos) + " (Esquiva e iniciativa)", 10);
     Estilo::impressaoEscrita(Estilo::AZUL + "INTELIGENCIA: " + Estilo::RESET + std::to_string(inteligencia) + " (Invasao de sistemas)", 10);
-    Estilo::impressaoEscrita(Estilo::AZUL + "TECNICA:      " + Estilo::RESET + std::to_string(tecnica) + " (Engenharia e tecnologia)", 10);
+    Estilo::impressaoEscrita(Estilo::AZUL + "TECNICA:      " + Estilo::RESET + std::to_string(tecnica) + " (Engenharia e armas de fogo)", 10);
     Estilo::impressaoEscrita(Estilo::AZUL + "MORAL:        " + Estilo::RESET + std::to_string(moral) + " (Controle e furtividade)", 10);
     Estilo::impressaoEscrita(Estilo::AZUL + "SORTE:        " + Estilo::RESET + std::to_string(sorte) + " (Criticos e milagres)", 10);
     Estilo::impressaoEscrita(Estilo::AZUL + "CARISMA:      " + Estilo::RESET + std::to_string(carisma) + " (Relacionamentos e persuasao)", 10);
@@ -39,4 +130,5 @@ int Personagem::getTecnica() { return tecnica; }
 int Personagem::getMoral() { return moral; }
 int Personagem::getSorte() { return sorte; }
 int Personagem::getCarisma() { return carisma; }
+Item Personagem::getArmaEquipada() { return armaEquipada; }
 
